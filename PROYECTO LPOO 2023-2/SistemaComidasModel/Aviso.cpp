9e034acd9fafdbe6e0fd1cdac12a665c7ab62a17
LuisCastro1904 <142554151+LuisCastro1904@users.noreply.github.com>
2023-09-21 00:41:44 -0500

    Delete PROYECTO LPOO 2023-2 directory

--- PROYECTO LPOO 2023-2/SistemaComidasModel/Aviso.cpp
+++
@@ -1,9 +0,0 @@
-#include "Aviso.h"
-
-Aviso::Aviso() {
-
-}
-Aviso::Aviso(int codigo, bool Estado) {
-	this->codigo = codigo;
-	this->Estado = Estado;
-}
--- PROYECTO LPOO 2023-2/SistemaComidasModel/Cocinero.cpp
+++
@@ -1,9 +0,0 @@
-#include "Cocinero.h"
-
-Cocinero::Cocinero() :Persona() {
-
-}
-Cocinero::Cocinero(int NumeroPedido) :Persona(codigo, Nombres, Apellidos, DNI)
-{
-	this->NumeroPedido = NumeroPedido;
-}
